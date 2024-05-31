86a7d2be8f4d73772c83e8c239cbe1f020e242ad
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-31 17:24:54 -0400

    Added the starter files to begin the portfolio.

---
+++ portfolio/src/components/Page/index.jsx
@@ -0,0 +1,14 @@
+import {Outlet} from 'react-router-dom';
+import PageContent from '../PageContent';
+
+function Page() {
+
+    return (
+        <section>
+            <PageContent>
+                <Outlet />
+            </PageContent>
+        </section>
+    );
+}
+export default Page;
