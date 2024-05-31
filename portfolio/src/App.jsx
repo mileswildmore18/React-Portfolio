453be81ac273a4856f4b6a3313b5634917d9aa09
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-31 19:12:22 -0400

    Added imports for the App jsx file with the connections of the header and navigation.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,29 @@
-import Navbar from '/components/Nav'
-import Header from '/components/Header'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from '/components/Nav';
+import Header from '/components/Header';
+import About from '/components/About';
+import Portfolio from '/components/Portfolio';
+import Contact from '/components/Contact';
+import Resume from '/components/Resume';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
+    <Router>
       <Header />
-      <Footer />
-    </>
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/" component={Portfolio} />
+        <Route path="/" component={Contact} />
+        <Route path="/" component={Resume} />
+      </Switch>
     
+      <Navbar />
+      <Footer />
+      
+
+    </Router>
+
   )
 }
 
