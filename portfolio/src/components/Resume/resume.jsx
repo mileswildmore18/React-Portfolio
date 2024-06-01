7d3b94c32060a07f1bc85044e2b7f2b44238aa73
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-01 13:40:01 -0400

    Setup the Resume page where people can see my resume and skills.

---
+++ portfolio/src/components/Resume/resume.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Resume from '';
+
+const Resume = () => {
+    return (
+        <header>
+            <h1>Resume</h1>
+            
+        </header>
+    );
+};
+
+export default Resume;
