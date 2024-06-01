757216330efc80d3ee371d1b2df4f6410ae8940c
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-01 11:30:10 -0400

    Added the portfolio code to show the main Projects done so far. Also added the project js file to connect to the portfolio.

---
+++ portfolio/src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Project from '/components/Project';
+
+const projectsData = [
+    {
+        title: 'The Wild Wild Wellness',
+        description: 'Get fit with exercises and check out healthy diets to get you into shape',
+        imageUrl: 'https://github.com/mileswildmore18/The-Wild-Wild-Wellness/raw/main/Assets/Wild%20Wild%20Wellness%20screenshot.png',
+        projectUrl: 'https://mileswildmore18.github.io/The-Wild-Wild-Wellness/'
+    },
+    {
+        title: 'XP Archive',
+        description: 'Pick out your favourite games and gain XP as you play the game and add them to your collection',
+        imageUrl: 'https://github.com/mileswildmore18/XP-Archive/raw/main/public/images/XP%20Archive%20Screenshot.png',
+        projectUrl: 'https://the-wild-coders.onrender.com/'
+    },
+    {
+        title: 'Project 3 coming soon!',
+        description: 'Coming soon!',
+        imageUrl: 'https://static01.nyt.com/images/2022/04/04/multimedia/15ai-nocode/15ai-nocode-videoSixteenByNineJumbo1600.jpg',
+        projectUrl: 'Coming Soon!'
+    }
+];
+
+const Portfolio = () => {
+    return (
+        <div className='portfolio'>
+            <h1>My Projects</h1>
+            {projectsData.map((project, index) => (
+                <Project
+                key={index}
+                title={project.title}
+                description={project.description}
+                imageUrl={project.imageUrl}
+                projectUrl={project.projectUrl}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Portfolio;
---
+++ portfolio/src/components/Project/Project.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Project = ({ title, description, imageUrl, projectUrl }) => {
+    return (
+        <div className="project">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {imageUrl && <img src={imageUrl} alt={title} />}
+            {projectUrl && <a href={projectUrl} target="_blank" rel="noopener noreferrer">View Project</a>}
+        </div>
+    );
+};
+
+export default Project;
