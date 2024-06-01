6eacd66eae2ba27318c91edcfc23a9f96c76f4a4
Miles Wildmore <mileswildmore18@gmail.com>
2024-05-31 21:37:05 -0400

    Added Navigation bar on the Nav jsx file as well as connecting it to the header jsx file.

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,11 +1,15 @@
-export default function About() {
+import React from 'react';
+
+const About = () => {
     return (
         <div>
             <h1>About Me</h1>
             <img src='assets/images/miles-picture.jpg'></img>
             <p>
-                My name is Miles Wildmore. I am a front end web developer who is passionate for developing websites for people. I specialize in making the pages as decorative and professional 
+                My name is Miles Wildmore. I am a front end web developer who is passionate for developing websites for people. I specialize in making the pages as decorative and professional.
+                Originally born in England, but now live in America. 
             </p>
         </div>
     )
 }
+export default About;
---
+++ portfolio/src/components/Nav/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Navigation = () => {
+    return (             
+            <nav>
+                <ul>
+                    <li><Link to="/">About Me</Link></li>
+                    <li><Link to="/Portfolio">Portfolio</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
+                    <li><Link to="/Resume">Resume</Link></li>
+                </ul>
+            </nav>
+    
+    );
+};
+
+export default Navigation;
